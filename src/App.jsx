12e6d69943c65ebb54f164a209e1e840d2616381
seyedalihosseini <122220669+seyedalihosseiniFrontDeveloper@users.noreply.github.com>
2024-03-07 20:48:56 +0330

    header success

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header/header";
+import CategoryList from "./Components/CategoryList/categoryList";
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <CategoryList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
